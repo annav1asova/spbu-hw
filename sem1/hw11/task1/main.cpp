2e2575ff0af07b4a1d3b39feecf21028a89777ae
annav1asova <annasv209@gmail.com>
2016-12-12 04:02:25 +0300

    fixed strange bugs

--- sem1/hw11/task1/main.cpp
+++ sem1/hw11/task1/main.cpp
@@ -24,73 +24,73 @@ bool isE(char s)
 
 bool isCorrectDouble (char string[])
 {
-    int sost = 0;
+    int state = 0;
     for (int i = 0; i < strlen(string); i++)
     {
         char symb = string[i];
-        switch (sost)
+        switch (state)
         {
             case 0:
                 if (isSign(symb))
-                    sost = 1;
+                    state = 1;
                 else if (isDigit(symb))
-                    sost = 2;
+                    state = 2;
                 else
                     return false;
                 break;
             case 1:
                 if (isDigit(symb))
-                    sost = 2;
+                    state = 2;
                 else
                     return false;
                 break;
             case 2:
                 if (isDigit(symb))
-                    sost = 2;
+                    state = 2;
                 else if (isDot(symb))
-                    sost = 3;
+                    state = 3;
                 else if (isE(symb))
-                    sost = 5;
+                    state = 5;
                 else
                     return false;
                 break;
             case 3:
                 if (isDigit(symb))
-                    sost = 4;
+                    state = 4;
                 else
                     return false;
                 break;
             case 4:
                 if (isDigit(symb))
-                    sost = 4;
+                    state = 4;
                 else if (isE(symb))
-                    sost = 5;
+                    state = 5;
                 else
                     return false;
                 break;
             case 5:
                 if (isSign(symb))
-                    sost = 6;
+                    state = 6;
                 else if (isDigit(symb))
-                    sost = 7;
+                    state = 7;
                 else
                     return false;
                 break;
             case 6:
                 if (isDigit(symb))
-                    sost = 7;
+                    state = 7;
                 else
                     return false;
                 break;
             case 7:
                 if (isDigit(symb))
-                    sost = 7;
+                    state = 7;
                 else
                     return false;
                 break;
         }
     }
-    if (sost == 2 || sost == 4 || sost == 7)
+    if (state == 2 || state == 4 || state == 7)
         return true;
     else
         return false;
@@ -98,13 +98,10 @@ bool isCorrectDouble (char string[])
 
 int main()
 {
-    for (int i = 0; i < 25; i++)
-    {
     char string[100] = {""};
     cin >> string;
     if (isCorrectDouble(string))
         cout << "Your string is correct real number" << endl;
     else
         cout << "Your string isn't correct real number" << endl;
-    }
 }
