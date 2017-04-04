4c9c9e8433e6c11a3bb86f217fe30e258ce39cb8
annav1asova <annasv209@gmail.com>
2017-04-04 23:47:35 +0300

    added javadoc

--- sеm2/hw3/task2/src/sem2/hw3/task2/Output.java
+++ sеm2/hw3/task2/src/sem2/hw3/task2/Output.java
@@ -4,5 +4,11 @@ import java.io.IOException;
 
 /** Interface that prints two-dimensional matrix */
 public interface Output {
+    /**
+     * prints two-dimensional matrix by spiral
+     *
+     * @param array
+     * @throws IOException
+     */
     void output(int[][] array) throws IOException;
 }
