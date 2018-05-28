18b1b39c823e2459d5327e91041fbdfb43fc8d99
annav1asova <annasv209@gmail.com>
2018-05-28 19:55:03 +0300

    added monoid function

--- sem4/hw5/task3/5_3.hs
+++ sem4/hw5/task3/5_3.hs
@@ -1,4 +1,5 @@
+import Control.Monad
+
 findElement:: Ord a => [a] -> Maybe a
-findElement (x:y:z:list) | y > x && y > z = Just y
-                         | otherwise = findElement (y:z:list)                   
+findElement (x:y:z:list) = mplus (if y > x && y > z then Just y else Nothing) $ findElement (y:z:list)                   
 findElement _ = Nothing
